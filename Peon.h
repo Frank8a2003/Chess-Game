86d56097184a2d97438f031664859429154cbb8a
Frank8a2003 <104097510+Frank8a2003@users.noreply.github.com>
2023-01-19 14:36:03 -0600

    Update Peon.h

--- Peon.h
+++ Peon.h
@@ -4,13 +4,14 @@
 
 class Peon: public Pieza{
   public:
-    
     Peon(string etiqueta, int contador, string color, int x, int y,bool avanceDoble);
-    bool validarMovimiento(int x, int y,Pieza* Tablero[][8])override;
+    bool validarMovimiento(int x, int y,Pieza* Tablero[][8]) override;
     bool validarComidaPaso(int,int, Pieza* Tablero[][8]);
     bool getAvanceDoble();
     void setAvanceDoble();
     void setAvanceNormal();
+    string imprime() override;
+
 private:
   bool avanceDoble;
 };
